3d7737314ac4afceaa43f6958098260d85868810
otissv <otissv@gmail.com>
2015-12-12 11:15:29 +0100

    added uery data atrribute methods

--- packages/react-uikit-base/src/helpers.js
+++ packages/react-uikit-base/src/helpers.js
@@ -24,6 +24,27 @@ const breakpoints = () => {
 };
 
 
+const cleanClasses = (c) => {
+  const str = ufunc.cleanAll(c).join(' ').trim();
+  return ufunc.maybeIf(str)(str !== '');
+};
+
+
+const closeClass = (alt) => {
+  return alt ? 'uk-close-alt' : ' uk-close';
+};
+
+const colSpan = [
+  '1-1',
+  '1-2',
+  '1-3', '2-3',
+  '1-4', '2-4', '3-4',
+  '1-5', '2-5', ' 3-5', '4-5',
+  '1-6', '2-6', ' 3-6', '4-6', '5-6',
+  '1-10', '2-10', '3-10', '4-10', '5-10', '6-10', '7-10', '8-10', '9-10'
+];
+
+
 const stringToClasses = (str, cssClasses) => {
   let classes = str.split(' ').map(item => {
     return cssClasses[item];
@@ -43,33 +64,23 @@ const getClass = (str, cssClasses) => {
 };
 
 
-const closeClass = (alt) => {
-  return alt ? 'uk-close-alt' : ' uk-close';
+const queryData = (value) => {
+  return document.querySelector(`[data-kitid="${value}"]`);
 };
 
 
-const cleanClasses = (c) => {
-  const str = ufunc.cleanAll(c).join(' ').trim();
-  return ufunc.maybeIf(str)(str !== '');
+const queryDataAll = (value) => {
+  return document.querySelectorAll(`[data-kitid="${value}"]`);
 };
-
-
-const colSpan = [
-  '1-1',
-  '1-2',
-  '1-3', '2-3',
-  '1-4', '2-4', '3-4',
-  '1-5', '2-5', ' 3-5', '4-5',
-  '1-6', '2-6', ' 3-6', '4-6', '5-6',
-  '1-10', '2-10', '3-10', '4-10', '5-10', '6-10', '7-10', '8-10', '9-10'
-];
 
 
 export default {
   breakpoints,
-  getClasses,
-  getClass,
   closeClass,
   colSpan,
-  cleanClasses
+  cleanClasses,
+  getClasses,
+  getClass,
+  queryData,
+  queryDataAll
 };
