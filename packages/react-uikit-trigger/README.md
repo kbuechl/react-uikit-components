742d5b6061d1cc7b403b47851fa824c4fdfb7faf
otissv <otissv@gmail.com>
2015-12-31 21:17:06 +0100

    updated readme

--- packages/react-uikit-trigger/README.md
+++ packages/react-uikit-trigger/README.md
@@ -1,1 +1,8 @@
-React uikit dropdown component alpha
+<div><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.24.3/css/uikit.almost-flat.min.css"/><section><h1>React Uikit Trigger</h1><p><a href="http://otissv.github.io/react-uikit-components/">http://otissv.github.io/react-uikit-components</a> for docs.</p></section><section><h2>Usage</h2><pre><code>npm install react-uikit-trigger --save;
+
+// ES6
+import uikit from &#x27;react-uikit-trigger&#x27;;
+
+// ES5
+var Dropdown = require(&#x27;react-uikit-trigger&#x27;);
+</code></pre></section></div>
--- packages/react-uikit-trigger/docs/trigger-doc.jsx
+++ packages/react-uikit-trigger/docs/trigger-doc.jsx
@@ -3,19 +3,6 @@
 
 import React from 'react';
 import Codeblock from 'react-uikit-codeblock';
-import Dropdown from '../lib/dropdown';
-import Table from 'react-uikit-table';
-import velocity from 'velocity-animate';
-
-
-const animateIn = (element) => {
-  velocity(element, {opacity: 1}, {display: 'block'}, 200);
-};
-
-
-const animateOut = (element) => {
-  velocity(element, {opacity: 0}, { display: 'none' }, 200);
-};
 
 
 export default class TriggerDoc extends React.Component {
@@ -24,9 +11,6 @@ export default class TriggerDoc extends React.Component {
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.24.3/css/uikit.almost-flat.min.css" />
       <section>
         <h1>React Uikit Trigger</h1>
-        <p className='uk-article-lead'>
-          Defines different styles for list dropdownigations.
-        </p>
         <p>
           <a href='http://otissv.github.io/react-uikit-components/'>http://otissv.github.io/react-uikit-components</a> for docs.
         </p>
@@ -46,21 +30,6 @@ var Dropdown = require('react-uikit-trigger');
 `}
         </Codeblock>
 
-        <hr className="uk-article-divider" />
-
-      </section>
-
-
-      <section>
-        <Dropdown
-          kitid='drop1'
-          trigger={{
-            body:'dropdown',
-            animate: {in: animateIn, out: animateOut}
-          }}
-        >
-          this is a drop down
-        </Dropdown>
       </section>
 
 
