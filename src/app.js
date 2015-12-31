2f786c4a955710057bbd36f2fab8407d2baf11c7
otissv <otissv@gmail.com>
2016-01-01 00:22:29 +0100

    fix script

--- src/app.js
+++ src/app.js
@@ -4,11 +4,11 @@ import React from 'react';
 import { Router } from 'react-router';
 import routes from './routes.jsx';
 import ReactDOM from 'react-dom';
-import createBrowserHistory from 'history/lib/createBrowserHistory';
+
 
 const mountNode = document.getElementById('mount-node');
 
 ReactDOM.render(
-  <Router history={ createBrowserHistory()} routes={routes}/>,
+  <Router routes={routes}/>,
   mountNode
 );
