222a6d73dedd0e0cc70a934ae03f346aed712e46
otissv <otissv@gmail.com>
2016-04-24 10:13:05 +0200

    react-router browserHistory

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import React from 'react';
-import { Router } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 import routes from './routes.jsx';
 import ReactDOM from 'react-dom';
 
@@ -9,6 +9,6 @@ import ReactDOM from 'react-dom';
 const mountNode = document.getElementById('mount-node');
 
 ReactDOM.render(
-  <Router routes={routes}/>,
+  <Router history={browserHistory} routes={routes}/>,
   mountNode
 );
