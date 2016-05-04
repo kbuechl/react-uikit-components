8c6d9b7a1aac0a96e9b7f946a8501f76dd07653e
otissv <otissv@gmail.com>
2016-05-04 12:48:12 +0200

    changed to pure component

--- packages/react-uikit-comment-list/src/comment-list.jsx
+++ packages/react-uikit-comment-list/src/comment-list.jsx
@@ -4,28 +4,33 @@ import React from 'react';
 import uikit from 'react-uikit-base';
 
 
-class CommentList extends React.Component {
-  render () {
-    const props = this.props;
+const CommentList = (props) => {
 
-    // CSS classes
-    const cssClassNames = uikit.helpers.cleanClasses([
-      'uk-comment-list',
-      props.classes,
-      props.className
-    ]);
+  // CSS classes
+  const cssClassNames = uikit.helpers.cleanClasses([
+    'uk-comment-list',
+    props.classes,
+    props.className
+  ]);
 
 
-    // Return Component
-    return <ul
-      {...props}
-      {...uikit.events(props)}
-      className={cssClassNames}
-      data-kitid={props.kitid}
-    >
-      {props.children}
-    </ul>;
-  }
-}
+  // Return Component
+  return <ul
+    {...props}
+    {...uikit.events(props)}
+    className={cssClassNames}
+    data-kitid={props.kitid}
+  >
+    {props.children}
+  </ul>;
+
+};
+
+CommentList.propTypes = {
+  children  : React.PropTypes.any,
+  className : React.PropTypes.string,
+  classes   : React.PropTypes.array,
+  kitid     : React.PropTypes.string
+};
 
 export default uikit.base(CommentList);
