e30f56e0bfa4b4e55205944bf55011bf4c2d280d
otissv <otissv@gmail.com>
2016-05-04 17:56:21 +0200

    new dom & string methods

--- packages/react-uikit-overlay/__tests__/helpers/renderElement.js
+++ packages/react-uikit-overlay/__tests__/helpers/renderElement.js
@@ -1,16 +1,32 @@
 'use strict';
 
-import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { createRenderer } from 'react-addons-test-utils';
 import dom from 'cheerio';
 
 
-export default function (element, tag) {
+export function htmlToDom (element) {
+  const $ = dom.load(element);
+
+  return {
+    dom : tag => tag ? $(tag) : $
+  };
+}
+
+export function htmlToString (element) {
+  return htmlToDom(element).dom().html().replace(/\n\s+/g,'');
+}
+
+
+export function reactToDom (element, tag) {
   const renderMarkup = ReactDOMServer.renderToStaticMarkup;
   const $ = dom.load(renderMarkup(element));
 
   return {
     dom : tag => tag ? $(tag) : $
   };
-};
+}
+
+export function reactToString (element) {
+  return reactToDom(element).dom().html();
+}
