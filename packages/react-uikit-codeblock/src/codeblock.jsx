670d393713bf0c60d204d1164102f696c669fa21
Otis Virginie <otis@kolabdigital.com>
2016-08-18 14:06:07 +0200

    fixed clean props

--- packages/react-uikit-codeblock/src/codeblock.jsx
+++ packages/react-uikit-codeblock/src/codeblock.jsx
@@ -2,22 +2,37 @@
 'use strict';
 
 import React from 'react';
-import uikit from 'react-uikit-base';
+import {
+  base,
+  events,
+  helpers
+} from 'react-uikit-base';
 
 
 const Codeblock = (props) => {
   // CSS classes
-  const cssClassNames = uikit.helpers.cleanClasses([
+  const cssClassNames = helpers.cleanClasses([
     props.classes,
     props.lang,
     props.className
   ]);
 
+  // Remove non valid html attributes
+  const ignoreProps = [
+    'children',
+    'classes',
+    'kitid',
+    'lang',
+    'syntax',
+  ];
+
+  const cleanProps = helpers.cleanProps(ignoreProps)(props);
+
 
   // Return Component
   return <pre
-    {...props}
-    {...uikit.events(props)}
+    {...cleanProps}
+    {...events(props)}
     className={cssClassNames}
     data-kitid={props.kitid}
   >
@@ -33,8 +48,9 @@ Codeblock.propTypes = {
   className : React.PropTypes.string,
   classes   : React.PropTypes.array,
   kitid     : React.PropTypes.string,
-  lang      : React.PropTypes.string
+  lang      : React.PropTypes.string,
+  syntax    : React.PropTypes.string
 };
 
 
-export default uikit.base(Codeblock);
+export default base(Codeblock);
