8c3393258a36c47e20f892b7388617b4ab4e8c25
Otis Virginie <otis@kolabdigital.com>
2016-08-18 18:15:16 +0200

    added utiliy to export

--- packages/react-uikit-base/src/uikit.jsx
+++ packages/react-uikit-base/src/uikit.jsx
@@ -4,16 +4,18 @@ import Helpers from './helpers';
 import Element from './element';
 import Base from './base';
 import Events from './events';
-
+import Utility from './utility';
 
 export const helpers = Helpers;
 export const element = Element;
 export const base = Base;
 export const events = Events;
+export const utility = Utility;
 
 export default {
   helpers: Helpers,
   element: Element,
   base: Base,
-  events: Events
+  events: Events,
+  utility: Utility
 };
