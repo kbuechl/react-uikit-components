d05297ba0f4052cadb7b0b8dd4b18ce9b9b64826
otissv <otissv@gmail.com>
2016-11-04 09:45:41 +0100

    first commit

---
+++ packages/react-uikit-progress/README.md
@@ -0,0 +1,45 @@
+#React UIkit Codeblock
+
+Displays code inside of a block.
+
+See [http://otissv.github.io/react-uikit-components/](http://otissv.github.io/react-uikit-components) for docs.
+
+##Usage
+The master branch has been updated to React 15. To use with React 0.14 checkout 0.14 branch.
+
+UIkit css is not included. You can get it from [getuikit.com](http://getuikit.com/)
+
+
+###Installation
+
+    npm install react-uikit-codeblock --save;
+
+    // ES6
+    import Codeblock from 'react-uikit-codeblock';
+
+    // ES5
+    var Codeblock = require('react-uikit-codeblock')
+
+###Example
+
+    <Codeblock>
+      {`<div>...<div>`};
+    </Codeblock>
+
+
+##Tests
+
+`npm run test`to run tests with minimal output.  
+`npm run test:spec` to run tests with detailed output.  
+`npm run test:watch` watches all directories and run tests with minimal output on file changes.
+
+##Build
+`npm run build` to build files fro distribution.  
+`npm run build:watch` watches src directory and builds files on changes.
+
+##Lint
+`npm run lint` lints scripts in src directory.  
+`npm run lint:watch` watches src directory and lints scripts in src directory.
+
+##License
+MIT
---
+++ packages/react-uikit-progress/__tests__/progress-unit-test.js
@@ -0,0 +1,208 @@
+'use strict';
+import test from 'tape';
+import Progress from '../lib/progress';
+import React from 'react';
+import {
+  htmlToString,
+  reactToDom,
+  reactToString
+} from './helpers/renderElement';
+
+
+test('Progress Component', nested => {
+  nested.test('Renders emtpy bar component:',
+    assert => {
+      const actual = reactToString(
+        <Progress
+          kitid='p1'
+          bar={0}
+        />
+      );
+
+
+      const expect = htmlToString(`
+        <div class="uk-progress" data-kitid="p1">
+          <div class="uk-progress-bar" style="width:0%;">0%</div>
+        </div>
+      `);
+
+
+      assert.equals(actual, expect,
+        'Correctly renders empty bar component');
+
+      assert.end();
+    });
+
+
+  nested.test('Renders component:',
+    assert => {
+      const actual = reactToString(
+        <Progress
+          kitid='p1'
+          bar={40}
+        />
+      );
+
+
+      const expect = htmlToString(`
+        <div class="uk-progress" data-kitid="p1">
+          <div class="uk-progress-bar" style="width:40%;">40%</div>
+        </div>
+      `);
+
+
+      assert.equals(actual, expect,
+        'Correctly renders component');
+
+      assert.end();
+    });
+
+
+    nested.test('size mini prop:',
+      assert => {
+        const actual = reactToString(
+          <Progress
+            kitid='p1'
+            bar={30}
+            size='mini'
+          />
+        );
+
+
+        const expect = htmlToString(`
+          <div class="uk-progress  uk-progress-mini" data-kitid="p1">
+            <div class="uk-progress-bar" style="width:30%;">30%</div>
+          </div>
+        `);
+
+
+        assert.equals(actual, expect,
+          'Correctly renders size mini prop');
+
+        assert.end();
+      });
+
+
+    nested.test('size small prop:',
+      assert => {
+        const actual = reactToString(
+          <Progress
+            kitid='p1'
+            bar={30}
+            size='small'
+          />
+        );
+
+
+        const expect = htmlToString(`
+          <div class="uk-progress  uk-progress-small" data-kitid="p1">
+            <div class="uk-progress-bar" style="width:30%;">30%</div>
+          </div>
+        `);
+
+
+        assert.equals(actual, expect,
+          'Correctly renders size small prop');
+
+        assert.end();
+      });
+
+
+  nested.test('context success prop:',
+    assert => {
+      const actual = reactToString(
+        <Progress
+          kitid='p1'
+          bar={30}
+          context='success'
+        />
+      );
+
+
+      const expect = htmlToString(`
+        <div class="uk-progress  uk-progress-success" data-kitid="p1">
+          <div class="uk-progress-bar" style="width:30%;">30%</div>
+        </div>
+      `);
+
+
+      assert.equals(actual, expect,
+        'Correctly renders context success prop');
+
+      assert.end();
+    });
+
+
+  nested.test('context danger prop:',
+    assert => {
+      const actual = reactToString(
+        <Progress
+          kitid='p1'
+          bar={30}
+          context='danger'
+        />
+      );
+
+
+      const expect = htmlToString(`
+        <div class="uk-progress  uk-progress-danger" data-kitid="p1">
+          <div class="uk-progress-bar" style="width:30%;">30%</div>
+        </div>
+      `);
+
+
+      assert.equals(actual, expect,
+        'Correctly renders context danger prop');
+
+      assert.end();
+    });
+
+  nested.test('context warning prop:',
+    assert => {
+      const actual = reactToString(
+        <Progress
+          kitid='p1'
+          bar={30}
+          context='warning'
+        />
+      );
+
+
+      const expect = htmlToString(`
+        <div class="uk-progress  uk-progress-warning" data-kitid="p1">
+          <div class="uk-progress-bar" style="width:30%;">30%</div>
+        </div>
+      `);
+
+
+      assert.equals(actual, expect,
+        'Correctly renders context warning prop');
+
+      assert.end();
+    });
+
+
+  nested.test('striped prop:',
+    assert => {
+      const actual = reactToString(
+        <Progress
+          kitid='p1'
+          bar={30}
+          striped={true}
+        />
+      );
+
+
+      const expect = htmlToString(`
+        <div class="uk-progress  uk-progress-striped" data-kitid="p1">
+          <div class="uk-progress-bar" style="width:30%;">30%</div>
+        </div>
+      `);
+
+
+      assert.equals(actual, expect,
+        'Correctly renders striped prop');
+
+      assert.end();
+    });
+});
