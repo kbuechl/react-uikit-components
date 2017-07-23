28d9e6f28f70c4ae1796afad9178da549e8c74df
Murray Lucas <murray.lucas@orionhealth.com>
2017-07-23 17:59:57 +1200

    Update nav package to contain website docs.
    * Add generated readme
    * Update unit test script runner location.
    * Add doc file from website to this package.
    * Update package.json with repo link, new npm scripts and cleaned dependancies
    
    Issue: None,
    Reviewer: PR

--- packages/react-uikit-nav/README.md
+++ packages/react-uikit-nav/README.md
@@ -1,71 +1,187 @@
-# React UIkit Nav
+# React Uikit Nav
 
-Defines different styles for list navigations in React using the UIKit CSS Framework.
-
-See [the website](http://otissv.github.io/react-uikit-components) for docs.
+Defines different styles for list navigations.
 
 ## Usage
 
-The master branch has been updated to React 15. To use with React 0.14 checkout 0.14 branch.
-UIkit css is not included. You can get it from [getuikit.com](http://getuikit.com/)
-
-### Installation
-
-```bash
-npm install react-uikit-nav --save
 ```
-```js
+# To install in your project: 
+npm install react-uikit-nav --save;
+```
+
+```
 // ES6
-import Nav from 'react-uikit-nav';
-import NavItem from 'react-uikit-nav/lib.nav-item';
+import Nav from 'react-uikit-nav'; 
+import NavItem from 'react-uikit-nav/lib/nav-item';
 
 // ES5
 var Nav = require('react-uikit-nav').default;
 var NavItem = require('react-uikit-nav/lib/nav-item').default;
 ```
 
-### Examples
+## Nav
 
-```js
-<Nav parent type='side' cols='1-4'>
-  <NavItem label='Active' href='#' active />
-  <NavItem parent label='Item' href='#' >
-    <Nav type='sub'>
-      <NavItem label='Sub item' href='#' />
-      <NavItem label='Sub item' href='#' />
-    </Nav>
-  </NavItem>
-  <NavItem label='Item' href='#' />
+Navigation consists of two components `<Nav>` and `<NavItem>`. The Nav component displays a list of links for various types of Navs. To choose a type of Nav the `type` prop must be added to the Nav component
+
+## Nav Side
+
+Add the `type="side"` prop to place a nav inside your sidebar, panels or anywhere else in your content.
+
+### Code
+
+```
+<Grid>
+  <Nav type='side' cols='1-4'>
+    <NavItem label='Active' href='#' active />
+    <NavItem parent label='Item' href='#' >
+        <Nav type='sub'>
+          <NavItem label='Sub item' href='#' />
+          <NavItem label='Sub item' href='#' />
+        </Nav>
+      </NavItem>
+    <NavItem label='Item' href='#' />
+    <NavItem label='Item' href='#' />
+  </Nav>
+</Grid>
+```
+
+## Navs Nested
+
+Navs can easily be nested insde of each other by using the Nav `parent` and `sub` props and the NavItems `parent` prop.
+
+### Code
+
+```
+<Grid>
+  <Nav  parenttype='side' cols='1-4'>
+    <NavItem label='Active' href='#' active />
+    <NavItem label='Item' href='#' />
+    <NavItem label='Item' href='#' />
+    <NavItem label='Item' href='#' />
+  </Nav>
+</Grid>
+```
+
+## Nav Dropdown
+
+Nav compnents can be placed into a Dropdown component by setting `type='dropdown'`
+
+Props can be passed to the Dropdown in the form of JSON object via the `dropdown` prop. See Droppdown component props for more details.
+
+### Code
+
+```
+
+<Droppdown trigger={{
+  body: 'Nav dropdown',
+  animate: {
+    in: (element) => this.animateIn(element),
+    out: (element) => this.animateOut(element)
+  }
+}}>
+  <Nav parent type='dropdown' cols='1-4'>
+    <NavItem body='Item' href='#' />
+    <NavItem body='Another item' href='#' />
+    <NavItem type='header' body='Header'/>
+    <NavItem body='Item' href='#' />
+    <NavItem body='Another item' href='#' />
+  </Nav>
+</Droppdown>
+```
+
+## Nav Items
+
+The NavItem component creates a Nav item which can either be a link, header or a divider. This can be specified using the `type` prop. The default type is link.
+
+| Prop | Description |
+| --- | --- |
+| `body` | Provides a body for a link. |
+| `href` | Provides a URL for a link. |
+| `active` | Shows the item as active. |
+| `subtitle` | Creates a subtitle. |
+| `type='link'` | Creates a link NavItem. |
+| `type='header'` | Creates a header NavItem. |
+| `type='body'` | Creates a body a link. |
+| `type='divider'` | Creates a divider NavItem for separating menu items. |
+
+### Code
+
+```
+
+<Grid indent>
+  <Nav type='side' cols='1-4'>
+    <NavItem type='header' body='Header'/>
+    <NavItem body='Active' href='#' active />
+    <NavItem body='Item' href='#' />
+    <NavItem body='Item' href='#' subtitle='Subtitle'/>
+    <NavItem body='Item' href='#' />
+    <NavItem type='divider' />
+    <NavItem body='Item' href='#' />
+  </Nav>
+</Grid>
+```
+
+## Nav Item collections
+
+An array of objects can be passed to the `items` prop of a Nav to generate NavItems
+
+### Code
+
+```
+
+<Grid indent>
+  <Nav cols='1-4' type='side' items={[{body: 'Home', href: '#', active: true}, {body: 'About', href: '#'}]} />
+</Grid>
+```
+
+## React Router
+
+NavItem can be used with [React Router](http://rackt.github.io/react-router/) by adding `<Link>` as a child of `<NavItem>`
+
+See [React Router](http://rackt.github.io/react-router/) for router configuration
+
+### Code
+
+```
+<Nav type='side'>
+  <NavItem><Link to="app">Home</Link></NavItem>
 </Nav>
 ```
 
-```js
-<Nav cols='1-4' type='side' items={[
-  {
-    label: 'Home',
-    href: '#',
-    active: true
-  },
-  {
-    label: 'About',
-    href: '#'
-  }
-]} />
-```
+## Nav Props
+
+See base component for additional utility props.
+
+| Prop | Type |
+| --- | --- |
+| parent | bool |
+| type | oneOf: 'dropdown' | 'side' | 'sub' |
+
+## Nav Props
+
+See Base for additional utility props.
+
+| Prop | Type |
+| --- | --- |
+| body | string |
+| href | string |
+| active | bool |
+| subtitle | string |
+| type | oneOf: 'link' | 'header' | 'body' | 'divider' |
 
 ## Tests
 
-`npm run test` to run tests with minimal output.  
-`npm run test:spec` to run tests with detailed output.  
-`npm run test:watch` watches all directories and run tests with minimal output on file changes.
+`npm run test` to run tests with minimal output.  
+`npm run test:spec` to run tests with detailed output.  
+`npm run test:watch` watches all directories and run tests with minimal output on file changes.  
 
 ## Build
-`npm run build` to build files from distribution.  
-`npm run build:watch` watches src directory and builds files on changes.
+`npm run build` to build files from distribution.  
+`npm run build:watch` watches src directory and builds files on changes.  
 
 ## Lint
-`npm run lint` lints scripts in src directory.  
-`npm run lint:watch` watches src directory and lints scripts in src directory.
+`npm run lint` lints scripts in src directory.  
+`npm run lint:watch` watches src directory and lints scripts in src directory.  
 
 ## License
 MIT
--- packages/react-uikit-nav/docs/nav-doc.jsx
+++ packages/react-uikit-nav/docs/nav-doc.jsx
@@ -1,350 +1,275 @@
-'use strict';
-
-
-import React from 'react';
-import Codeblock from 'react-uikit-codeblock';
+import React, { PropTypes } from 'react';
+import Droppdown from 'react-uikit-dropdown';
+import { helpers } from 'react-uikit-base';
 import Grid from 'react-uikit-grid';
-import Nav from '../lib/nav';
-import NavItem from '../lib/nav-item';
 import Table from 'react-uikit-table';
 
+import Nav from '../lib/nav';
+import NavItem from '../lib/nav-item';
 
-export default class NavDoc extends React.Component {
-  render () {
-    return <div>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.24.3/css/uikit.almost-flat.min.css" />
-      <section>
-        <h1>React Uikit Nav</h1>
-        <p className='uk-Nav-lead'>
-          Defines different styles for list navigations.
-        </p>
-        <p>
-          <a href='http://otissv.github.io/react-uikit-components/'>http://otissv.github.io/react-uikit-components</a> for docs.
-        </p>
-      </section>
+import DocHeader from '../../script-utils/lib/DocHeader';
+import DocExample from '../../script-utils/lib/DocExample';
+import DocFooter from '../../script-utils/lib/DocFooter';
 
-      <section>
-        <h2>Usage</h2>
+let velocity;
+if (process.env.NODE_ENV !== 'mdGenerator') {
+  velocity = require('velocity-animate');
+} else {
+  velocity = () => {};
+}
 
-        <Codeblock>
-{`npm install react-uikit-nav --save;</code>
+const name = 'Nav';
+const npmName = 'react-uikit-nav';
+const secondaryName = 'NavItem';
+const secondaryPath = 'react-uikit-nav/lib/nav-item';
+const summary = 'Defines different styles for list navigations.';
+const propTuples = [{
+    Prop: "body",
+    Type: "string",
+  }, {
+    Prop: "href",
+    Type: "string",
+  }, {
+    Prop: "active",
+    Type: "bool",
+  }, {
+    Prop: "subtitle",
+    Type: "string",
+  }, {
+    Prop: "type",
+    Type: "oneOf: 'link' / 'header' / 'body' / 'divider'",
+}];
 
-// ES6
-Nav uikit from 'react-uikit-nav';
-NavItem uikit from 'react-uikit-nav/lib/na-vitem';
+const handleParentClick = (e) => {
+  e.preventDefault();
+  const element = helpers.getElement('sub');
 
-// ES5
-var Nav = require('react-uikit-nav');
-var NavItem = require('react-uikit-nav/lib/na-vitem');`}
-        </Codeblock>
-
-        <hr className="uk-article-divider" />
-
-        <p>
-          Navigation consists of two components <code>&lt;Nav&gt;</code> and <code>&lt;NavItem&gt;</code>.
-          The Nav component displays a list of links for various types of Navs.
-          To choose a type of Nav the <code>type</code> prop must be added to the Nav component.
-        </p>
-
-      </section>
-
-
-      <section>
-        <h2>Nav side</h2>
-
-          <p>
-            Add the <code>type="side"</code> prop to place a nav inside your sidebar,
-            panels or anywhere else in your content.
-          </p>
-
-          <h3 className='example'>Example</h3>
-
-          <Grid>
-            <Nav type='side' cols='1-4'>
-              <NavItem label='Active' href='#' active />
-              <NavItem label='Item' href='#' />
-              <NavItem label='Item' href='#' />
-            </Nav>
-          </Grid>
-
-        <h4 className='code'>Code</h4>
-        <Codeblock syntax='xml'>
-{
-`<Grid>
-  <Nav type='side' cols='1-4'>
-    <NavItem label='Active' href='#' active />
-    <NavItem label='Item' href='#' />
-    <NavItem label='Item' href='#' />
-  </Nav>
-</Grid>
-`}
-        </Codeblock>
-      </section>
-
-
-      <section>
-        <h2>Navs nested</h2>
-          <p>
-            Navs can easily be nested insde of each other by using the
-            Nav <code>parent</code> and <code>sub</code> props and
-            the NavItems <code>parent</code> prop.
-          </p>
-
-          <h3 className='example'>Example</h3>
-          <Grid>
-            <Nav parent type='side' cols='1-4'>
-              <NavItem label='Active' href='#' active />
-              <NavItem parent label='Item' href='#' >
-                <Nav type='sub'>
-                  <NavItem label='Sub item' href='#' />
-                  <NavItem label='Sub item' href='#' />
-                </Nav>
-              </NavItem>
-              <NavItem label='Item' href='#' />
-            </Nav>
-          </Grid>
-
-          <h4 className='code'>Code</h4>
-          <Codeblock syntax='xml'>
-  {
-`<Grid>
-  <Nav parent type='side' cols='1-4'>
-    <NavItem label='Active' href='#' active />
-    <NavItem parent label='Item' href='#' >
-      <Nav type='sub'>
-        <NavItem label='Sub item' href='#' />
-        <NavItem label='Sub item' href='#' />
-      </Nav>
-    </NavItem>
-    <NavItem label='Item' href='#' />
-  </Nav>
-</Grid>
-`}
-          </Codeblock>
-        </section>
-
-
-        <section>
-          <h2>Nav dropdown</h2>
-          <p>
-            Coming soon...
-          </p>
-
-          <h3 className='example'>Example</h3>
-            <Grid>
-              <Nav type='dropdown' items={[
-                {
-                  label: 'Home',
-                  href: '#',
-                  active: true
-                },
-                {
-                  label: 'About',
-                  href: '#'
-                }
-              ]}/>
-            </Grid>
-
-          <h4 className='code'>Code</h4>
-          <Codeblock syntax='xml'>
-  {`
-  `}
-          </Codeblock>
-        </section>
-
-
-        <section>
-          <h2>Nav offcanvas</h2>
-          <p>
-            Coming soon...
-          </p>
-
-          <h3 className='example'>Example</h3>
-
-          <h4 className='code'>Code</h4>
-          <Codeblock syntax='xml'>
-  {`
-  `}
-          </Codeblock>
-        </section>
-
-
-        <section>
-          <h2>Nav accordion</h2>
-          <p>
-            Coming soon...
-          </p>
-
-          <h3 className='example'>Example</h3>
-
-          <h4 className='code'>Code</h4>
-          <Codeblock syntax='xml'>
-  {`
-  `}
-          </Codeblock>
-        </section>
-
-
-        <section>
-          <h2>Nav Items</h2>
-            <p>
-              The NavItem component creates a Nav item which can either be a link, header or a divider.
-              This can be specified using the <code>type</code> prop. The default type is link.
-            </p>
-
-            <Table>
-              <thead>
-                <tr>
-                  <th className='uk-text-left'>Type</th>
-                  <th className='uk-text-left'>Descrition</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td className='uk-text-left'><code>link</code></td>
-                  <td className='uk-text-left'>
-                    Add the link <code>type</code> to create a link<br />
-                    Add the <code>label</code> to provide a label for a link.<br />
-                    Add the <code>href</code> props to provide a URL for a link.<br />
-                    Add the <code>active</code>  prop to show the active as active.<br />
-                  Add the <code>subtitle='*'</code> prop to create a subtitle.
-                  </td>
-                </tr>
-                <tr>
-                  <td className='uk-text-left'><code>header</code></td>
-                  <td className='uk-text-left'>
-                    Add the header <code>type</code> to a to create a header.<br />
-                    Add the <code>label</code> to provide a label for a link.
-                  </td>
-                </tr>
-                <tr>
-                  <td className='uk-text-left'><code>divider</code></td>
-                  <td className='uk-text-left'>Add the divider <code>type</code> to create a divider separating menu items.</td>
-                </tr>
-              </tbody>
-            </Table>
-
-            <h3 className='example'>Example</h3>
-
-            <Grid>
-              <Nav type='side' cols='1-4'>
-                <NavItem type='header' label='Header'/>
-                <NavItem label='Active' href='#' active />
-                <NavItem label='Item' href='#' />
-                <NavItem label='Item' href='#' subtitle='Subtitle'/>
-                <NavItem label='Item' href='#' />
-                <NavItem type='divider' />
-                <NavItem label='Item' href='#' />
-              </Nav>
-            </Grid>
-
-          <h4 className='code'>Code</h4>
-          <Codeblock syntax='xml'>
-{
-`<Grid>
-  <Nav type='side' cols='1-4'>
-    <NavItem type='header' label='Header'/>
-    <NavItem label='Active' href='#' active />
-    <NavItem label='Item' href='#' />
-    <NavItem label='Item' href='#' subtitle='Subtitle'/>
-    <NavItem label='Item' href='#' />
-    <NavItem type='divider' />
-    <NavItem label='Item' href='#' />
-  </Nav>
-</Grid>
-`}
-          </Codeblock>
-        </section>
-
-        <section>
-          <h2> Nav Item collections</h2>
-          <h3 className='example'>Example</h3>
-          <Grid>
-            <Nav cols='1-4' type='side' items={[
-              {
-                label: 'Home',
-                href: '#',
-                active: true
-              },
-              {
-                label: 'About',
-                href: '#'
-              }
-            ]} />
-          </Grid>
-
-          <h4 className='code'>Code</h4>
-          <Codeblock syntax='xml'>
-{
-`<Grid>
-  <Nav cols='1-4' type='side' items={[
+  velocity(
+    element, { height: '100%' },
     {
-      label: 'Home',
-      href: '#',
-      active: true
+      // display: 'none',
+      visibility: 'visible'
+    },
+    200
+  );
+};
+
+const generateCodeSnippet = ({ parent }) =>
+`<Grid>
+  <Nav ${parent ? " parent": ""}type='side' cols='1-4'>
+    <NavItem label='Active' href='#' active />
+    ${parent ?
+      "<NavItem label='Item' href='#' />":
+      `<NavItem parent label='Item' href='#' >
+        <Nav type='sub'>
+          <NavItem label='Sub item' href='#' />
+          <NavItem label='Sub item' href='#' />
+        </Nav>
+      </NavItem>`
+    }
+    <NavItem label='Item' href='#' />
+    <NavItem label='Item' href='#' />
+  </Nav>
+</Grid>`;
+
+const generateExampleJSX = ({ parent }) => (
+  <Grid>
+    <Nav parent={parent} type='side' cols='1-4'>
+      <NavItem label='Active' href='#' active />
+      <NavItem label='Item' href='#'>
+        { parent &&
+          <Nav type='sub'>
+            <NavItem label='Sub item' href='#' />
+            <NavItem label='Sub item' href='#' />
+          </Nav>
+        }
+      </NavItem>
+      <NavItem label='Item' href='#' />
+    </Nav>
+  </Grid>
+);
+generateExampleJSX.propTypes = {
+  parent: PropTypes.bool,
+};
+
+const sideExample = generateExampleJSX({ parent: false });
+const sideSnippet = generateCodeSnippet({ parent: false });
+
+const navsNestedExample = generateExampleJSX({ parent: true });
+const navsNestedSnippet = generateCodeSnippet({ parent: true });
+
+const dropdownExample = (
+  <Droppdown trigger={{
+    body: 'Nav dropdown',
+    animate: {
+      in: (element) => this.animateIn(element),
+      out: (element) => this.animateOut(element)
+    }
+  }}>
+    <Nav parent type='dropdown' cols='1-4'>
+      <NavItem body='Item' href='#' />
+      <NavItem body='Another item' href='#' />
+      <NavItem type='header' body='Header'/>
+      <NavItem body='Item' href='#' />
+      <NavItem body='Another item' href='#' />
+    </Nav>
+  </Droppdown>
+);
+const dropdownSnippet = `
+<Droppdown trigger={{
+  body: 'Nav dropdown',
+  animate: {
+    in: (element) => this.animateIn(element),
+    out: (element) => this.animateOut(element)
+  }
+}}>
+  <Nav parent type='dropdown' cols='1-4'>
+    <NavItem body='Item' href='#' />
+    <NavItem body='Another item' href='#' />
+    <NavItem type='header' body='Header'/>
+    <NavItem body='Item' href='#' />
+    <NavItem body='Another item' href='#' />
+  </Nav>
+</Droppdown>`;
+
+const navItemTable = (
+<Table
+  head={['Prop', 'Description']}
+  body={[{
+    Prop : <code>body</code>,
+    Description : "Provides a body for a link."
+  }, {
+    Prop : <code>href</code>,
+    Description : "Provides a URL for a link."
+  }, {
+    Prop : <code>active</code>,
+    Description : "Shows the item as active."
+  }, {
+    Prop : <code>subtitle</code>,
+    Description : "Creates a subtitle."
+  }, {
+    Prop : <code>type='link'</code>,
+    Description : "Creates a link NavItem."
+  }, {
+    Prop : <code>type='header'</code>,
+    Description : "Creates a header NavItem."
+  }, {
+    Prop : <code>type='body'</code>,
+    Description : "Creates a body a link."
+  }, {
+    Prop : <code>type='divider'</code>,
+    Description : "Creates a divider NavItem for separating menu items."
+  }]} />
+);
+const navItemExample = (
+  <Grid indent>
+    <Nav type='side' cols='1-4'>
+      <NavItem type='header' body='Header'/>
+      <NavItem body='Active' href='#' active />
+      <NavItem body='Item' href='#' />
+      <NavItem body='Item' href='#' subtitle='Subtitle'/>
+      <NavItem body='Item' href='#' />
+      <NavItem type='divider' />
+      <NavItem body='Item' href='#' />
+    </Nav>
+  </Grid>
+);
+const navItemSnippet = `
+<Grid indent>
+  <Nav type='side' cols='1-4'>
+    <NavItem type='header' body='Header'/>
+    <NavItem body='Active' href='#' active />
+    <NavItem body='Item' href='#' />
+    <NavItem body='Item' href='#' subtitle='Subtitle'/>
+    <NavItem body='Item' href='#' />
+    <NavItem type='divider' />
+    <NavItem body='Item' href='#' />
+  </Nav>
+</Grid>`;
+
+const navItemCollectionExample = (
+  <Grid indent>
+    <Nav cols='1-4' type='side' items={[{body: 'Home', href: '#', active: true}, {body: 'About', href: '#'}]} />
+  </Grid>
+);
+const navItemCollectionSnippet = `
+<Grid indent>
+  <Nav cols='1-4' type='side' items={[{body: 'Home', href: '#', active: true}, {body: 'About', href: '#'}]} />
+</Grid>`;
+
+const navPropsTable = (
+  <Table head={['Prop', 'Type']} body={[
+    {
+      Prop: 'parent',
+      Type: 'bool'
     },
     {
-      label: 'About',
-      href: '#'
+      Prop: 'type',
+      Type: "oneOf: 'dropdown' / 'side' / 'sub'"
     }
-  ]} />
-</Grid>
-`}
-          </Codeblock>
-        </section>
+  ]}
+/>);
 
+{/* // TODO: could this be done outside of the class with a generated click handler...? */}
 
-        <section>
-          <h2>React Router</h2>
-          <p>
-            NavItem can be used with <a href='http://rackt.github.io/react-router/'>React Router</a> by
-            adding <code>&lt;Link&gt;</code> as a child of <code>&lt;NavItem&gt;</code>.
-          </p>
+const NavDoc = (props) => (
+  <div>
+    <DocHeader {... { name, npmName, secondaryName, secondaryPath, summary }} />
+    <DocExample
+      name="Nav"
+      description={<span>Navigation consists of two components <code>&lt;Nav&gt;</code> and <code>&lt;NavItem&gt;</code>. The Nav component displays a list of links for various types of Navs. To choose a type of Nav the <code>type</code> prop must be added to the Nav component</span>}
+    />
+    <DocExample
+      name="Nav Side"
+      description={<span>Add the <code>type="side"</code> prop to place a nav inside your sidebar, panels or anywhere else in your content.</span>}
+      jsx={sideExample}
+      snippet={sideSnippet}
+    />
+    <DocExample
+      name="Navs Nested"
+      description={<span>Navs can easily be nested insde of each other by using the Nav <code>parent</code> and <code>sub</code> props and the NavItems <code>parent</code> prop.</span>}
+      jsx={navsNestedExample}
+      snippet={navsNestedSnippet}
+    />
+    <DocExample
+      name="Nav Dropdown"
+      description={(<div>
+        <p>Nav compnents can be placed into a Dropdown component by setting <code>type='dropdown'</code></p>
+        <p>Props can be passed to the Dropdown in the form of JSON object via the <code>dropdown</code> prop. See Droppdown component props for more details.</p>
+      </div>)}
+      jsx={dropdownExample}
+      snippet={dropdownSnippet}
+    />
+    <DocExample
+      name="Nav Items"
+      description={<span>The NavItem component creates a Nav item which can either be a link, header or a divider. This can be specified using the <code>type</code> prop. The default type is link.</span>}
+      table={navItemTable}
+      jsx={navItemExample}
+      snippet={navItemSnippet}
+    />
+    <DocExample
+      name="Nav Item collections"
+      description={<span>An array of objects can be passed to the <code>items</code> prop of a Nav to generate NavItems</span>}
+      jsx={navItemCollectionExample}
+      snippet={navItemCollectionSnippet}
+    />
+    <DocExample
+      name="React Router"
+      description={(
+        <div>
+          <p>NavItem can be used with <a href='http://rackt.github.io/react-router/'>React Router</a> by adding <code>&lt;Link&gt;</code> as a child of <code>&lt;NavItem&gt;</code></p>
+          <p>See <a href='http://rackt.github.io/react-router/'>React Router</a> for router configuration</p>
+        </div>)
+      }
+      snippet={`<Nav type='side'>\n  <NavItem><Link to="app">Home</Link></NavItem>\n</Nav>`}
+    />
+    <DocExample
+      name="Nav Props"
+      description="See base component for additional utility props."
+      table={navPropsTable}
+    />
+    <DocFooter { ...{ name, propTuples }} />
+  </div>
+);
 
-          <p>
-            See <a href='http://rackt.github.io/react-router/'>React Router</a> for router configuration.
-          </p>
-
-          <h4 className='code'>Code</h4>
-          <Codeblock syntax='xml'>
-{
-`<Nav type='side'>
-  <NavItem><Link to="app">Home</Link></NavItem>
-</Nav>
-`}
-          </Codeblock>
-        </section>
-
-
-      <section>
-        <h2>Nav Props</h2>
-        <p>
-
-        See base component for additional utility props.
-        </p>
-
-        <Table>
-          <thead>
-            <tr>
-              <th className='uk-text-left'>Prop</th>
-              <th className='uk-text-left'>Type</th>
-              <th className='uk-text-left'>Value</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className='uk-text-left'>
-                <code></code>
-              </td>
-              <td className='uk-text-left'></td>
-              <td className='uk-text-left'></td>
-            </tr>
-
-          </tbody>
-        </Table>
-      </section>
-
-
-    </div>;
-  }
-}
+export default NavDoc;
